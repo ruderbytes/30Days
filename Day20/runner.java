a2cf1ff920211502441e54a2cfd24f1c616fb461
Prakhar <mywolfdale@gmail.com>
2016-12-27 22:14:32 +0530

    MinMax Value

---
+++ Day20/runner.java
@@ -0,0 +1,9 @@
+class runner{
+	public static void main(String[] args) throws Exception{
+		int a = Integer.MIN_VALUE;
+		int x = Integer.MAX_VALUE;
+		
+		int b = -a;
+		System.out.println( a+ " "+b+ " "+x );
+}
+}
