25518b3e0adf3d75ec7a5d35f0d65b6b0c7b0edd
Prakhar Gupta <mywolfdale@gmail.com>
2016-12-28 22:15:03 +0530

    Create trick.java

---
+++ Day21/trick.java
@@ -0,0 +1,10 @@
+class MainClass
+{
+    public static void main(String[] args)
+    {
+        int i = 10 + + 11 - - 12 + + 13 - - 14 + + 15;
+ 
+        System.out.println(i);
+        // 75 as -(-x) = x
+    }
+}
