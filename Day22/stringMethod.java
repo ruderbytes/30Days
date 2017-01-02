152a3107546c84587fe000525460ccfef55f3c89
Prakhar <mywolfdale@gmail.com>
2017-01-02 21:17:16 +0530

    String Method

---
+++ Day22/stringMethod.java
@@ -0,0 +1,10 @@
+class TestClass{
+	public static void main(String args []){
+		String s = "MINIMUM";
+		System.out.println(s.substring(4, 7)); //1
+		System.out.println(s.substring(5)); //2
+		System.out.println(s.substring(s.indexOf('I', 3))); //3
+		// Exception is Thrown
+		System.out.println(s.substring(s.indexOf('I', 4))); //4
+	}
+}
