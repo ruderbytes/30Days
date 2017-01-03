e1bf21a6893641ecacf97d736561d3b665081108
Prakhar Gupta <mywolfdale@gmail.com>
2017-01-03 09:37:04 +0530

    Create NullPtrExp.java

---
+++ Day23/NullPtrExp.java
@@ -0,0 +1,20 @@
+class ExceptionHandling
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This statement will be executed"); 
+ 
+        String s = null;
+ 
+        try
+        {
+            System.out.println(s.length());  //This statement throws NullPointerException
+        }
+        catch (Exception e)
+        {
+            System.out.println("Exception Caught");
+        }
+ 
+        System.out.println("Now, This statement will also be executed");
+    }
+}
