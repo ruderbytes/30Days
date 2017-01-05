e9b38d72c355a53310008472103ae9929dc61564
Prakhar <mywolfdale@gmail.com>
2017-01-05 22:02:56 +0530

    Unreachable catch

---
+++ Day25/unreachable.java
@@ -0,0 +1,65 @@
+//This code will not compile
+class runner{
+	public static void main(String[] args){
+
+	String [] s = {"abc","3",null,"ninja"};
+	
+	for(int i=0; i<7; i++){
+		try{
+			int a = s[i].length() + Integer.parseInt(s[i]);
+		}
+		// Broader Exception def.
+		catch(Exception x){
+			System.out.println("Catch Null ptr");
+		}
+		//Unreachable catch statements - Compile error
+		catch(ArrayIndexOutOfBoundsException x){
+			System.out.println("Arr out of Bound");
+		}
+		catch(NumberFormatException x){
+			System.out.println("Number format Exp");
+		}
+			
+	}
+
+	
+   }		
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
