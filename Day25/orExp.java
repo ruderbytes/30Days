1bc21b8adddd0b0bfbc19fe7a700f6e6a9940802
Prakhar <mywolfdale@gmail.com>
2017-01-05 21:59:41 +0530

    ORing

---
+++ Day25/orExp.java
@@ -0,0 +1,67 @@
+class runner{
+	public static void main(String[] args){
+
+	String [] s = {"abc","3",null,"ninja"};
+	
+	for(int i=0; i<7; i++){
+		try{
+			int a = s[i].length() + Integer.parseInt(s[i]);
+		}
+		catch(NullPointerException | ArrayIndexOutOfBoundsException | NumberFormatException ex){
+			System.out.println("All together");
+		}
+
+/*
+		catch(NullPointerException x){
+			System.out.println("Catch Null ptr");
+		}
+		catch(ArrayIndexOutOfBoundsException x){
+			System.out.println("Arr out of Bound");
+		}
+		catch(NumberFormatException x){
+			System.out.println("Number format Exp");
+		}
+*/			
+	}
+
+	
+   }		
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
