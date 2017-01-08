ae626fab198d9d11fbeb6b1ba24a033b51c1dde1
Prakhar <mywolfdale@gmail.com>
2017-01-08 12:03:05 +0530

    Memeber Inner class

---
+++ Day26/memeberInnerClass/innerClass3.java
@@ -0,0 +1,64 @@
+//Non static Inner Class
+// Member Inner class
+
+
+// MIC Must only contain Non-static members.
+
+
+class outer{
+	static int avg;
+	int x = 10;
+	
+	void show_beta(){
+		//create a instance of inner clas
+		memberInnerClass inner = new memberInnerClass();
+		System.out.println(inner.beta);
+	}
+
+	class memberInnerClass{
+		// Compile time error	
+	//	static int a;
+	//	static void alpha(){}	
+		int beta = 64;
+		int x = 70;
+		//Member inner class can have final static members
+		static final int variable = 100;
+		// Can access outer class variables	
+		void alpha(){
+			outer o = new outer();
+			System.out.println("Parameter of Outer class"+o.x);
+			System.out.println("Inside Inner class x"+x);
+			System.out.println("Static final variable in inner class"+variable);
+		}
+
+
+
+
+	}
+}
+
+class runner{
+	public static void main(String[] args){
+		
+		//Instantiate outer class obj 
+		outer outObj = new outer();
+		//Instantiate Inner class obj 
+		outer.memberInnerClass in = outObj.new memberInnerClass();
+		outObj.show_beta();		
+		in.alpha();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
