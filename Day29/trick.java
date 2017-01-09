ee770a63c4ed31b91b3c8c731d246d5706bd74b2
Prakhar <mywolfdale@gmail.com>
2017-01-09 21:51:01 +0530

    Java Trick

---
+++ Day29/trick.java
@@ -0,0 +1,12 @@
+class runner{
+	public static void main(String[] args){
+		//Guess output
+		System.out.println(""+9+2+"");
+		System.out.println(foo()+9+2);
+		System.out.println(9+2+foo());
+		//System.out.println(""+9+2+"");
+	}
+	static String foo(){
+		return "Hola";
+	}
+}	
