a2d60719363707a86cdb8be81adba1643df0b707
Prakhar <mywolfdale@gmail.com>
2017-01-11 22:20:23 +0530

    Obj referneces

---
+++ Day30/trick2.java
@@ -0,0 +1,13 @@
+//Never forget
+class runner{
+public static void main(String[] args){
+		Object obj1 = new Object();
+		Object obj2 = obj1;
+		if( obj1.equals(obj2) ){
+			 System.out.println("true");
+		}
+		else{
+		 System.out.println("false");
+		}
+	}
+}
