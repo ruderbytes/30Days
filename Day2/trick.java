335c6c085f6bd5ab96303c1b086d8e11160808fd
Prakhar <mywolfdale@gmail.com>
2017-01-15 10:37:21 +0530

    Trick

---
+++ Day2/trick.java
@@ -0,0 +1,13 @@
+//comand line args [java runner --0.5]
+class runner{
+	public static int getSwitch(String str){
+		return (int) Math.round( Double.parseDouble(str.substring(1, str.length()-1)));
+	}
+	public static void main(String args []){
+		switch(getSwitch(args[0])){
+			case 0 : System.out.print("Hello ");
+			case 1 : System.out.print("World"); break;
+			default : System.out.print(" Good Bye");
+		}
+	}		
+}
